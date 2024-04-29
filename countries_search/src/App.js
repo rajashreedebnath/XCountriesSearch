7d872f4eca0064bbdca102a5bbcc848fcfe7981f
rajashreedebnath <rajashreedebnath29@gmail.com>
2024-04-30 05:02:30 +0530

    Test

--- countries_search/src/App.js
+++ countries_search/src/App.js
@@ -1,25 +1,100 @@
-import logo from './logo.svg';
+import React, {useEffect, useState} from "react";
 import './App.css';
 
-function App() {
+
+
+export default function Country() {
+
+  const [countries, setCountries] = useState([]);
+
+  const [filteredCountries, setFilteredCountries] = useState([]);
+  const [searchTerm, setSearchTerm] = useState('');
+  const [error, setError] = useState(null);
+
+
+
+  useEffect(() => {
+
+    fetch("https://restcountries.com/v3.1/all")
+    .then((res) => res.json())
+    .then((data) => {
+      setCountries(data);
+      setFilteredCountries(data);
+    })
+    .catch((error) => {
+      setError(error)
+      console.error("Error fetching countries:", error)
+    });
+
+  }, []);
+
+
+
+
+  useEffect(() => {
+
+    if(!searchTerm) {
+
+      setFilteredCountries(countries);
+      return;
+    }
+    const filtered = countries.filter(country =>
+      country.name.common.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+    );
+    setFilteredCountries(filtered);
+  }, [searchTerm, countries]);
+
+
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+
+      <div className="search_container">
+        <input 
+          type="text"
+          placeholder="Search for countries..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search_input"
+        />
+      </div>
+
+
+
+      {error && <div className="error">Error: {error.message}</div>}
+
+
+
+      <div className="countries_container">
+
+        {filteredCountries.map((country) => (
+
+          <div key={country.cca3} className="card">
+
+            <img
+              src={country.flags.png}
+              alt={`Flag of ${country.name.common}`}
+              className="image"
+            />
+
+            <h2>{country.name.common}</h2>
+
+          </div>
+        ))}
+
+      </div>
+
     </div>
+    
   );
 }
 
-export default App;
+
